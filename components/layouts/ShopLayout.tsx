2878b4b8b4636b788211df31f222a21e2064c69b
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-05-16 12:42:55 -0500

    Before start new Layout Auth

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+import { Navbar, SideMenu } from "../ui";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<PropsWithChildren<Props>> = ({ children, title, pageDescription, imageFullUrl }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={pageDescription} />
+        <meta name='og:title' content={title} />
+        <meta name='og:description' content={pageDescription} />
+        {imageFullUrl && <meta name='og:image' content={imageFullUrl} />}
+      </Head>
+
+      <Navbar />
+
+      <SideMenu />
+
+      <main style={{ margin: "80px auto", maxWidth: "1140px", padding: "0px 30px" }}>{children}</main>
+
+      {/* Footer */}
+      <footer>{/* TODO: Custom footer */}</footer>
+    </>
+  );
+};
---
+++ pages/checkout/summary.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, Button, Card, CardContent, Divider, Grid, Typography, Link } from "@mui/material";
+import NextLink from "next/link";
+import { CartList, OrderSummary } from "../../components/cart";
+import { ShopLayout } from "../../components/layouts";
+
+const SummaryPage = () => {
+  return (
+    <ShopLayout title={"Resumen de orden"} pageDescription={"Resumen de la orden"}>
+      <Typography variant='h1' component='h1'>
+        Resumen de la orden
+      </Typography>
+
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h2'>Resumen (3 productos)</Typography>
+              <Divider sx={{ my: 1 }} />
+
+              <Box display='flex' justifyContent='space-between'>
+                <Typography variant='subtitle1'>Dirección de entrega</Typography>
+                <NextLink href='/checkout/address' passHref>
+                  <Link underline='always'>Editar</Link>
+                </NextLink>
+              </Box>
+
+              <Typography>Nombre nombre</Typography>
+              <Typography>323 algun lugar</Typography>
+              <Typography>Ciudad, HYA 234</Typography>
+              <Typography>Kuwait</Typography>
+              <Typography>+1 2323232333</Typography>
+
+              <Divider sx={{ my: 1 }}></Divider>
+
+              <Box display='flex' justifyContent='end'>
+                <NextLink href='/cart' passHref>
+                  <Link underline='always'>Editar</Link>
+                </NextLink>
+              </Box>
+
+              <OrderSummary />
+
+              <Box sx={{ mt: 3 }}>
+                <Button color='secondary' className='circular-btn' fullWidth>
+                  Confirmar Orden
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default SummaryPage;
